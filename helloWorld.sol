2651a9281fd76d12d6b840be7a3190501c1f54e6
Sambhav Jain <sambhavjain2612@gmail.com>
2018-02-18 17:52:20 +0530

    recompile wih constructor

--- helloWorld.sol
+++ helloWorld.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.18;
 contract MyContract {
     string word = "Hello World";
     
+    function MyContract(string myWord) public {
+        myWord = word;
+    }
+
     function my() public constant returns(string) {
         return word;
     }
